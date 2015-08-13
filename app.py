f55b00d359ab0f3f449b04607293f5e6afc6a9ce
Sharma <mail2naveensharma@gmail.com>
2015-08-13 22:32:36 +0530

    added test templates

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
 
+
+@app.route('/_add_numbers')
+def add_numbers():
+    """Add two numbers server side, ridiculous but well..."""
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return jsonify(result=a + b)
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
